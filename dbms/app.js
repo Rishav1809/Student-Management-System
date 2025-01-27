827d7f1354376c86d72b1f862b7f65dc70a22383
Rishav Prakash <prakashrishav18@gmail.com>
2025-01-27 21:09:30 +0530

    Add files via upload

---
+++ dbms/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+// app.use('/img', express.static(__dirname + 'public/img'))
+
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'html')
+
+app.get('/', (req, res) => {
+    res.render('feedback1')
+})
+app.post('', ()=>{
+    
+})
+
+
+
+app.get('/feedback2', (req, res) => {
+    res.render('feedback2')
+})
+
+app.listen(port, ()=> console.info('listening on 5000'));
